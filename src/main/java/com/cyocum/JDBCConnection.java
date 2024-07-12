a08bb96de868dd55256fb758c7286cbdbaf921e5
cyocum6 <cyocum6@unm.edu>
2024-07-12 12:37:15 -0600

    a

--- src/main/java/com/cyocum/JDBCConnection.java
+++ src/main/java/com/cyocum/JDBCConnection.java
@@ -73,7 +73,7 @@ public class JDBCConnection {
     // delete console from database
     public String deleteConsole(String id) {
         List<Window> list = getConsoles();
-        String insert = "delete from console where id = " + id;
+        String insert = "delete from TempData where id = " + id;
         try ( Connection conn = setupConnection()) {
             Statement statement = (Statement) conn.createStatement();
             statement.execute(insert);
