d7923b338a7c4f6b9721a2b11c8efd3b4ecff01d
cyocum6 <cyocum6@unm.edu>
2024-07-20 16:13:22 -0600

    Update Settings.java

--- proj/ChrisServer/src/main/java/com/cyocum/classes/Settings.java
+++ proj/ChrisServer/src/main/java/com/cyocum/classes/Settings.java
@@ -15,19 +15,19 @@ public class Settings{
         this.id = id;
     }
 
-    public String getTemp1() {
+    public int getTemp1() {
         return temp1;
     }
 
-    public void setTemp1(String temp1) {
+    public void setTemp1(int temp1) {
         this.temp1 = temp1;
     }
 
-    public String getTemp2() {
+    public int getTemp2() {
         return temp2;
     }
 
-    public void setTemp2(String temp2) {
+    public void setTemp2(int temp2) {
         this.temp2 = temp2;
     }
 
